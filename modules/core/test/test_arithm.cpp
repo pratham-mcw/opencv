3832985b55f1f2327e88792d69196b7b33cebe4e
pratham-mcw <pratham.kumar@multicorewareinc.com>
2026-01-06 22:45:46 +0530

    Update test_arithm.cpp

--- modules/core/test/test_arithm.cpp
+++ modules/core/test/test_arithm.cpp
@@ -1573,9 +1573,6 @@ class ElemWiseTest : public ::testing::TestWithParam<ElemWiseOpPtr> {};
 
 TEST_P(ElemWiseTest, accuracy)
 {
-    #if defined(_WIN32) && defined(_M_ARM64)
-        return ;
-    #endif
     ElemWiseOpPtr op = GetParam();
 
     int testIdx = 0;
